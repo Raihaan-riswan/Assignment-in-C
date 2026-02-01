3127399dfc677e7fdb40459e9a7333a20570c0be
Raihaan Riswan <mohamedraihaanmohamedriswan@gmail.com>
2026-02-01 20:40:58 +0530

    upload assignments

---
+++ Assignment06.c
@@ -0,0 +1,19 @@
+//This is Question NO:06
+
+#include <stdio.h>
+int main(){
+    int i, j, k ,rows;
+    printf("Enter rows :");
+    scanf("%d",&rows);
+
+    for(i=1; i<=rows; i++){
+        for(j=1; j<= rows - i; j++){
+            printf(" ");
+        }
+        for(k=1; k <= (2 * i - 1); k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
